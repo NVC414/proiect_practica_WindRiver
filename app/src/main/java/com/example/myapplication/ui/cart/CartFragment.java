f87dd1bf2949480ad5913a5b6e0690a652d3df59
Misaila Marius-Alessandru <misailaalessandru@gmail.com>
2025-06-30 13:41:11 +0300

    Cart finally works

--- app/src/main/java/com/example/myapplication/ui/cart/CartFragment.java
+++ app/src/main/java/com/example/myapplication/ui/cart/CartFragment.java
@@ -10,17 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.databinding.FragmentCartBinding;
 
 public class CartFragment extends Fragment
-    {
+{
 
+    private FragmentCartBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        CartViewModel cartViewModel = new ViewModelProvider(this).get(
                 CartViewModel.class);
 
+        binding = FragmentCartBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final TextView textView = binding.textCart;
+        cartViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
@@ -30,4 +36,4 @@ public class CartFragment extends Fragment
         super.onDestroyView();
         binding = null;
     }
-    }
+}
