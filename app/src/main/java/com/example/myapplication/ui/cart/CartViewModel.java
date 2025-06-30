74828b1dcac1823ae6bbdfb1de19dd35a9f8ade9
Misaila Marius-Alessandru <misailaalessandru@gmail.com>
2025-06-30 13:26:09 +0300

    Notifications -> Cart

--- app/src/main/java/com/example/myapplication/ui/cart/CartViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/cart/CartViewModel.java
@@ -1,15 +1,15 @@
-package com.example.myapplication.ui.notifications;
+package com.example.myapplication.ui.cart;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel
+public class CartViewModel extends ViewModel
     {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel()
+    public CartViewModel()
     {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
