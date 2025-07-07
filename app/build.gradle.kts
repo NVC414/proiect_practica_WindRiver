98519f0c95ba9a346a1e00fd230114cc4426a9b0
Nicolae Vladut-Cristian <129413672+NVC414@users.noreply.github.com>
2025-07-07 16:25:16 +0300

    firebase AI test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.ai)
+
 }
